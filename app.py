ac5622cbd6b5074bcf102b29d433a6b9fa501177
NicholasStambaugh <nastambaugh@gmail.com>
2023-03-31 15:44:12 -0400

    update #5

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
-# app.py
 from flask import Flask, render_template, request
-import requests
+from newsapi import NewsApiClient
 
 app = Flask(__name__)
+newsapi = NewsApiClient(api_key='f39095ae8be4476db173a4fafae9fe53')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        query = request.form.get('query')
-        api_key = 'f39095ae8be4476db173a4fafae9fe53'
-        url = f'https://newsapi.org/v2/everything?q={query}&apiKey={api_key}'
-        response = requests.get(url)
-        articles = response.json()['articles']
-        return render_template('index.html', articles=articles, query=query)
-    else:
-        return render_template('index.html')
+        query = request.form['query']
+        sort_by = request.form['sort_by']
+        articles = newsapi.get_everything(q=query, language='en', sort_by=sort_by)
+        return render_template('results.html', articles=articles['articles'])
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
