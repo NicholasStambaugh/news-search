cf444f0662459dc0bb96ee67dc7162584b7b992b
NicholasStambaugh <nastambaugh@gmail.com>
2023-03-31 14:12:59 -0400

    update #3

--- app.py
+++ app.py
@@ -1,29 +1,20 @@
+# app.py
 from flask import Flask, render_template, request
-from newsapi import NewsApiClient
+import requests
 
 app = Flask(__name__)
-newsapi = NewsApiClient(api_key='f39095ae8be4476db173a4fafae9fe53')
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        query = request.form['query']
-        articles = newsapi.get_everything(q=query, language='en', sort_by='relevancy')
-        return render_template('results.html', articles=articles['articles'])
-
-    return render_template('index.html')
-
-@app.route('/results', methods=['GET'])
-def results():
-    query = request.args.get('q')
-    api_key = newsapi
-    url = f'https://newsapi.org/v2/everything?q={query}&apiKey={api_key}'
-    response = requests.get(url)
-    articles = response.json()['articles']
-    return render_template('results.html', articles=articles)
-
+        query = request.form.get('query')
+        api_key = 'f39095ae8be4476db173a4fafae9fe53'
+        url = f'https://newsapi.org/v2/everything?q={query}&apiKey={api_key}'
+        response = requests.get(url)
+        articles = response.json()['articles']
+        return render_template('index.html', articles=articles, query=query)
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
